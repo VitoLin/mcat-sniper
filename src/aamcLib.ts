f8ec70bba1788c7b04d20e2181eaa6c8e64fc00b
Vito <65823917+VitoLin@users.noreply.github.com>
2024-12-28 20:04:13 -0800

    create functions for playwright steps

---
+++ src/aamcLib.ts
@@ -0,0 +1,124 @@
+import { BrowserContext, Page } from "playwright";
+import { sendMessage } from "./message";
+
+export async function goToSchedule(page: Page) {
+    await page.goto(
+        "https://students-residents.aamc.org/register-mcat-exam/register-mcat-exam"
+    );
+    await page.waitForLoadState("domcontentloaded");
+    await page
+        .getByRole("link", { name: "Register for the MCAT Exam" })
+        .click();
+
+    await page.waitForURL(
+        (url) => url.href.includes("login") || url.href.includes("dashboard"),
+        { timeout: 5000 }
+    );
+}
+
+export async function goToExamSearch(page: Page) {
+    await page.getByRole("button", { name: "Schedule an Exam" }).isVisible();
+    await page.getByRole("button", { name: "Schedule an Exam" }).click();
+
+    await page.getByRole("button", { name: "Agree" }).isVisible();
+    await page.getByRole("button", { name: "Agree" }).click();
+
+    await page.url().includes("SelectTestCenterAndDateProximity");
+}
+
+export async function attemptLogin(
+    page: Page,
+    username: string,
+    password: string
+) {
+    await page.waitForLoadState("domcontentloaded");
+    await page.getByPlaceholder("Enter User Name").isVisible();
+    await page.getByPlaceholder("Enter User Name").fill(username);
+
+    await page.getByPlaceholder("Enter Password").isVisible();
+    await page.getByPlaceholder("Enter Password").fill(password);
+
+    await page.getByRole("button", { name: "Sign In" }).isVisible();
+    await page.getByRole("button", { name: "Sign In" }).click();
+
+    await page.url().includes("dashboard");
+
+    saveAuth(page.context());
+}
+
+export async function searchForExam(
+    page: Page,
+    day: string,
+    month: string,
+    year: string,
+    address: string
+) {
+    if (address != "") {
+        await page.getByPlaceholder("Search by address").isVisible();
+        await page.getByPlaceholder("Search by address").fill(address);
+    }
+
+    await page.getByRole("img", { name: "Click for calendar" }).click();
+
+    let currentMonth: string =
+        (await page.locator("span.ui-datepicker-month").innerText()) || "";
+
+    while (!currentMonth.includes(month)) {
+        console.log("Current Month: ", currentMonth);
+        await page.getByRole("button", { name: "> Next Month" }).isVisible();
+        await page.getByRole("button", { name: "> Next Month" }).click();
+
+        currentMonth =
+            (await page.locator("span.ui-datepicker-month").innerText()) || "";
+    }
+
+    const regex = new RegExp(`${day}.*${month}.*${year}`);
+    await page.getByLabel(regex).isVisible();
+    await page.getByLabel(regex).click();
+}
+
+export async function keepSearching(
+    page: Page,
+    minTimeout: number,
+    maxTimeout: number,
+    numTopCloses: number
+) {
+    await page.getByRole("button", { name: "Search" }).isVisible();
+    await page.getByRole("button", { name: "Search" }).click();
+    await page.waitForLoadState("domcontentloaded");
+
+    await page.context().storageState({ path: "auth.json" });
+
+    let available = false;
+    for (let i = 0; i < numTopCloses; i++) {
+        console.log("Checking test center: ", i);
+        if (
+            !(await page.locator(`#testCenter_${i}`).innerText()).includes(
+                "None available"
+            )
+        ) {
+            available = true;
+            break;
+        } else {
+            console.log("Test Center ", i, " is not available");
+        }
+    }
+
+    if (available) {
+        console.log("MCAT Test Centers Available!");
+        sendMessage("MCAT Test Centers Available!");
+    } else {
+        console.log("No MCAT Test Centers Available");
+    }
+    await randomTimeout(minTimeout, maxTimeout);
+}
+
+function saveAuth(context: BrowserContext) {
+    context.storageState({ path: "auth.json" });
+}
+
+export function randomTimeout(min: number, max: number) {
+    return new Promise((resolve) =>
+        setTimeout(resolve, Math.random() * (max - min) + min)
+    );
+}
--- src/mcat.ts
+++ src/mcat.ts
@@ -1,99 +1,61 @@
-import { interceptors } from "undici-types";
-import { sendMessage } from "./message";
 import { BrowserContext, firefox } from "playwright";
+import {
+    goToExamSearch,
+    goToSchedule,
+    attemptLogin,
+    searchForExam,
+    keepSearching,
+} from "./aamcLib";
 
 require("dotenv").config({ path: "./secrets.env" });
 
 const username: string = process.env.MCAT_USERNAME || "";
 const password: string = process.env.MCAT_PASSWORD || "";
+const address: string = process.env.MCAT_ADDRESS || "";
+
+const day: string = "21";
+const month: string = "March";
+const year: string = "2025";
+
+const minTimeout: number = 60000;
+const maxTimeout: number = 120000;
+const numTopCloses: number = 2;
 
 (async () => {
     const browser = await firefox.launch({ headless: false });
     const context = await browser.newContext({ storageState: "./auth.json" });
-    const cookies = await context.cookies();
     const page = await context.newPage();
 
     await addStealth(context);
+
     try {
-        await page.goto(
-            "https://students-residents.aamc.org/register-mcat-exam/register-mcat-exam"
-        );
-        await page
-            .getByRole("link", { name: "Register for the MCAT Exam" })
-            .click();
-        await randomTimeout(1000, 2000);
+        await goToSchedule(page);
 
-        await page.waitForLoadState("domcontentloaded");
-        await randomTimeout(5000, 6000);
-
-        if (page.url().includes("login")) {
-            await page.getByPlaceholder("Enter User Name").fill(username);
-            await randomTimeout(1000, 2000);
-            await page.getByPlaceholder("Enter Password").fill(password);
-            await randomTimeout(1000, 2000);
-            await page.getByRole("button", { name: "Sign In" }).click();
-            await page.waitForLoadState("domcontentloaded");
-            await randomTimeout(3000, 5000);
+        // Check if already logged in, if not, attempt to login
+        if (page.url().includes("dashboard")) {
+            console.log("Already logged in");
+        } else if (await page.url().includes("login")) {
+            console.log("Logging in");
+            attemptLogin(page, username, password);
+        } else {
+            console.log("Error: Not on the expected page");
+            return -1;
         }
 
-        await page.getByRole("button", { name: "Schedule an Exam" }).click();
-        await randomTimeout(1000, 2000);
-        await page.getByRole("button", { name: "Agree" }).click();
-        await randomTimeout(1000, 2000);
+        await goToExamSearch(page);
 
-        await page.getByRole("img", { name: "Click for calendar" }).click();
-        await randomTimeout(1000, 2000);
-
-        if (page.getByRole("button", { name: "> Next Month, February" })) {
-            await page
-                .getByRole("button", { name: "> Next Month, February" })
-                .click();
-            await randomTimeout(1000, 2000);
-        }
-        if (page.getByRole("button", { name: "> Next Month, March" })) {
-            await page
-                .getByRole("button", { name: "> Next Month, March" })
-                .click();
-            await randomTimeout(1000, 2000);
-        }
-
-        await page.getByLabel("Friday 21st of March 2025").click();
-        await randomTimeout(1000, 2000);
+        await searchForExam(page, day, month, year, address);
 
         while (true) {
-            await page.getByRole("button", { name: "Search" }).click();
-            await randomTimeout(1000, 2000);
-
-            await page.waitForLoadState("domcontentloaded");
-            await randomTimeout(2000, 3000);
-
-            await context.storageState({ path: "auth.json" });
-
-            if (
-                !(await page.locator("#testCenter_0").innerText()).includes(
-                    "None available"
-                ) ||
-                !(await page.locator("#testCenter_1").innerText()).includes(
-                    "None available"
-                )
-            ) {
-                sendMessage("MCAT Test Centers Available!");
-            } else {
-                console.log("No MCAT Test Centers Available");
-            }
-            await randomTimeout(60000, 120000);
+            await keepSearching(page, minTimeout, maxTimeout, numTopCloses);
         }
     } catch (error) {
         console.error("An error occurred:", error);
 
-        // Take a screenshot on error
         const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
         const screenshotPath = `error-screenshot-${timestamp}.png`;
         await page.screenshot({ path: screenshotPath });
         console.log(`Screenshot saved: ${screenshotPath}`);
-
-        // Optional: Close browser after error
-        await browser.close();
     }
 })();
 
@@ -127,10 +89,4 @@ async function addStealth(context: BrowserContext) {
     for (let evasion of stealth.callbacks) {
         await context.addInitScript(evasion.cb as any, evasion.a);
     }
-}
-
-function randomTimeout(min: number, max: number) {
-    return new Promise((resolve) =>
-        setTimeout(resolve, Math.random() * (max - min) + min)
-    );
 }
